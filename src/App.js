fe3c3a48afc77328e26432dec1c0351e27e63e28
Msadoon94 <M.sadoon94@gmail.com>
2021-05-07 21:43:28 -0600

    Add http post request for profile creation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
 
 import ProfileCreation from './components/ProfileCreation';
+import Login from './components/Login';
 
 function App() {
     return (
@@ -10,11 +11,13 @@ function App() {
                     <main>
                         <nav>
                             <ul>
-                                <li><Link to="/profile-creation">Profile Creation</Link></li>
+                                <li><Link to="/cryptoProfile">Profile Creation</Link></li>
+                                <li><Link to="/login">Login</Link></li>
                             </ul>
                         </nav>
                         <Switch>
-                            <Route path="/profile-creation" component={ProfileCreation} />
+                            <Route path="/cryptoProfile" component={ProfileCreation} />
+                            <Route path="/login" component={Login}/>
                         </Switch>
                     </main>
                 </BrowserRouter>
--- src/components/ProfileCreation.js
+++ src/components/ProfileCreation.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
-class ProfileCreation extends React.Component{
+class ProfileCreation extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            pass: "",
+            message: ""
+        }
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.id]: e.target.value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+        axios.post("/cryptoProfile", this.state)
+            .then((response) => {
+                console.log(response.status);
+            }, (error) => {
+                console.log(error);
+            });
+    };
 
     render() {
-        return(
+        const {name, pass} = this.state;
+        return (
             <div>
                 <h1 className={"title"}>Profile Creation</h1>
-                <form>
-                    <label htmlFor={"pname"} >
-                        Profile Name
-                        <input type="text" id={"pname"} name={"pname"}/>
-                    </label><br/>
-                    <label htmlFor={"pass"}>
-                        Password
-                        <input type={"text"} id={"pass"} name={"pass"}/>
-                    </label><br/>
-                    <input type={"submit"} value={"Create New Profile"}/>
+                <form
+                    onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor={"name"}>
+                            Profile Name
+                            <input type="text" id={"name"} value={name}
+                                   onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label htmlFor={"pass"}>
+                            Password
+                            <input type={"text"} id={"pass"} value={pass}
+                                   onChange={this.handleChange}/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <input type={"submit"} id={"submit"} value={"Create New Profile"}/>
+                    </div>
                 </form>
             </div>
-            )
+        )
     }
 }
+
 export default ProfileCreation
